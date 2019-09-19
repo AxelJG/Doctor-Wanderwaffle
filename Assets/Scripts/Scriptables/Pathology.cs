d3eb2563d82e0775550bdfa3f2513297b6687916
AxelJG <axeljimenez33@gmail.com>
2019-09-19 20:59:04 +0200

    pathologies scriptable objects

---
+++ Assets/Scripts/Scriptables/Pathology.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Pathology", menuName = "Pathology")]
+public class Pathology : ScriptableObject {
+    public uint id;
+    public float timeOfLife;
+    public enum DifficultyState {very_easy, easy, medium, hard, very_hard };
+    public DifficultyState difficulty;
+    public List<Actuator> actuators;
+    public Sprite sprite;
+}
