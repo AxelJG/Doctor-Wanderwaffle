88a2146a7fcb145592284b9ee3aa0359f2221e20
AxelJG <axeljimenez33@gmail.com>
2019-09-21 15:23:44 +0200

    Pathologies notes menu

--- Assets/Scripts/Scriptables/Pathology.cs
+++ Assets/Scripts/Scriptables/Pathology.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Pathology", menuName = "Pathology")]
 public class Pathology : ScriptableObject {
     public uint id;
+    public string namePathology;
     public float timeOfLife;
     public enum DifficultyState {very_easy, easy, medium, hard, very_hard };
     public DifficultyState difficulty;
---
+++ Assets/Scripts/UI/NoteContent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteContent : MonoBehaviour
+{
+    public Pathology pathology;
+    public Text pathologyNameText, difficultyText;
+    public Image AspectImage;
+    public Transform curesTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPathologyNote();
+    }
+    private void LoadPathologyNote() {
+        pathologyNameText.text = pathology.namePathology;
+        string difficultyResult = pathology.difficulty.ToString();
+
+        if (difficultyResult.Equals("very_easy")) {
+            difficultyResult = "very easy";
+        }else if (difficultyResult.Equals("very_hard")) {
+            difficultyResult = "very hard";
+        }
+        difficultyText.text = difficultyResult;
+
+        AspectImage.sprite = pathology.sprite;
+
+        foreach (Transform cure in curesTransform) {
+            for (int i = 0; i < pathology.actuators.Count; i++) {
+                cure.GetComponent<Image>().sprite = pathology.actuators[i].sprite;
+            }
+        }
+    }
+}
